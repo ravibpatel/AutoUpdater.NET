546059a6f4035b568701299b634e770fb989d63f
Ravi Patel <ravi15.1990@hotmail.com>
2014-08-05 05:08:50 +0000

    Added Danish translation provided by Anders Klinting.

--- AutoUpdaterTest/Properties/AssemblyInfo.cs
+++ AutoUpdaterTest/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RBSoft")]
 [assembly: AssemblyProduct("AutoUpdaterTest")]
-[assembly: AssemblyCopyright("Copyright © RBSoft 2012")]
+[assembly: AssemblyCopyright("Copyright © 2012-2014 RBSoft")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
