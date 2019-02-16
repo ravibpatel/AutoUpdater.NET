6b5fd6621a687b190de135a7958e45cae79a529b
Oleg Varlamov <os.varlamov@gmail.com>
2019-02-16 14:10:42 +0600

    Added the ability to specify the size of the default update form (#234)

--- AutoUpdater.NET/AutoUpdater.cs
+++ AutoUpdater.NET/AutoUpdater.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Drawing;
 using System.Globalization;
 using System.IO;
 using System.Net;
@@ -198,6 +199,11 @@ namespace AutoUpdaterDotNET
         public static event ParseUpdateInfoHandler ParseUpdateInfoEvent;
 
         /// <summary>
+        ///     Set if you want the default update form to have a different size.
+        /// </summary>
+        public static Size? UpdateFormSize = null;
+
+        /// <summary>
         ///     Start checking for new version of application and display dialog to the user if update is available.
         /// </summary>
         /// <param name="myAssembly">Assembly to use for version checking.</param>
@@ -323,6 +329,11 @@ namespace AutoUpdaterDotNET
         public static void ShowUpdateForm()
         {
             var updateForm = new UpdateForm();
+            if (UpdateFormSize.HasValue)
+            {
+                updateForm.Size = UpdateFormSize.Value;
+            }
+
             if (updateForm.ShowDialog().Equals(DialogResult.OK))
             {
                 Exit();
