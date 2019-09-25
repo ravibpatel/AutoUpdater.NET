6f74e27b87173d0527e4e35c0859a64f354b05b0
jay1891 <jason.de.feyter@qisoftworks.be>
2019-09-25 16:10:20 +0200

    Merging finished

--- AutoUpdater.NET/AutoUpdater.cs
+++ AutoUpdater.NET/AutoUpdater.cs
@@ -932,27 +932,4 @@ namespace AutoUpdaterDotNET
         {
         }
     }
-
-    /// <summary>
-    ///     Provides Custom Authentication header for web request.
-    /// </summary>
-    public class CustomAuthentication : IAuthentication
-    {
-        private string HttpRequestHeaderAuthorizationValue { get; }
-
-        /// <summary>
-        /// Initializes authorization header value for Custom Authentication
-        /// </summary>
-        /// <param name="httpRequestHeaderAuthorizationValue">Value to use as http request header authorization value</param>
-        public CustomAuthentication(string httpRequestHeaderAuthorizationValue)
-        {
-            HttpRequestHeaderAuthorizationValue = httpRequestHeaderAuthorizationValue;
-        }
-
-        /// <inheritdoc />
-        public override string ToString()
-        {
-            return HttpRequestHeaderAuthorizationValue;
-        }
-    }
 }
