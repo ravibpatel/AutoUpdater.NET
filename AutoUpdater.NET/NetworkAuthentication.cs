2a2af4e0a2b369664295dbf4077c36f71549ccf5
Andrew Slobodyanuk <andrew0852@yandex.ru>
2020-08-24 11:45:11 +0300

    Implemented network authentication. (#388)

---
+++ AutoUpdater.NET/NetworkAuthentication.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+
+namespace AutoUpdaterDotNET
+{
+    /// <summary>
+    ///     Provides credentials for Network Authentication.
+    /// </summary>
+    public class NetworkAuthentication : IAuthentication
+    {
+        private string Username { get; }
+
+        private string Password { get; }
+
+        /// <summary>
+        ///     Initializes credentials for Network Authentication.
+        /// </summary>
+        /// <param name="username">Username to use for Network Authentication</param>
+        /// <param name="password">Password to use for Network Authentication</param>
+        public NetworkAuthentication(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public void Apply(ref MyWebClient webClient)
+        {
+            webClient.Credentials = new NetworkCredential(Username, Password);
+        }
+    }
+}
